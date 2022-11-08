65fe977e7097ec205f68a9809d2bf468048edaa5
Hailey Reent <haileyreent9@gmail.com>
2022-11-08 08:35:40 +0200

    cleaned code up

--- Question1/Answer1.js
+++ Question1/Answer1.js
@@ -2,15 +2,16 @@ let n = 15
 let output = []
 
 for (let i = 1 ; i<=n ; i++) {
-if (i % 3 == 0 && i % 5 == 0) {
-    output.push("Fizzbuzz")
-  }
- else if (i%3 == 0) {
-output.push('Fizz')
- }
- else if (i%5 == 0) {
-  output.push('buzz')
- }
- else output.push(i)
-}
+  if (i%3 == 0 && i%5 == 0) {
+    output.push('FizzBuzz')
+    }
+  else if (i%3 == 0) {
+    output.push('Fizz')
+    }
+  else if (i%5 == 0) {
+    output.push('Buzz')
+    }
+  else output.push(i)
+    }
+
 console.log(output)
